7fe7df239941476cdb7595836a2e3b57aab2914d
Isaac-Mander <ima6796@stacmail.net>
2022-03-29 19:58:06 +1300

    Pre image commit

--- index.php
+++ index.php
@@ -14,12 +14,14 @@
     <a href="https://io.adafruit.com/Greenbird2026/dashboards/co2-sensors">Air Quality</a>
     <a href="http://192.168.1.132">Desk Server</a>
     <a href="http://192.168.1.20/phpmyadmin/">phpMyAdmin</a>
-
     </div>
 
 
     <div class="Statistics">
-      
+
+    <div class="Box"><img src="" alt=""></div>
+
+
     </div>
 
     </div>
