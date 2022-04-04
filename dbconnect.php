ba6644cb5fd941afdb2a8897f6e3b408af0f3459
Isaac-Mander <ima6796@stacmail.net>
2022-04-04 12:58:29 +1200

    Added forum page

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,8 @@
 <?php
 // Connection details for our database
-  $blog_db = mysqli_connect('eu-cdbr-west-02.cleardb.net','b3f9053b24d2d9','359d6e2f','heroku_c0931c115c29e04');
+//Remote server
+$blog_db = mysqli_connect('eu-cdbr-west-02.cleardb.net','b3f9053b24d2d9','359d6e2f','heroku_c0931c115c29e04');
+
+//Local Server if remote unavailable
+//$blog_db = mysqli_connect('localhost','root','Peterjohn05','local-pi-web');
  ?>
--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
 <div class="Navbar">
     <a href="http://localhost/dashboard/PiWebsite">Home</a> 
     <a href="https://io.adafruit.com/Greenbird2026/dashboards/raspberrypi-test">Adafruit DeskPi</a>
-    <a href="#"></a>
+    <a href="forum_index.php">Forum</a>
     <a href="#"></a>
     <a href="http://192.168.1.20/phpmyadmin/">phpMyAdmin</a>
 </div>
